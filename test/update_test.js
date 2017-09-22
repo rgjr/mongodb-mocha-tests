be00aa9dc60295ce07c43041f67b96cc72560ade
Raul Gutierrez <rdgutierrez@gmail.com>
2017-09-22 12:41:45 -0700

    added validation tests

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe("Updating records", () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: "Joe" });
+    joe = new User({ name: "Joe", postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -36,5 +36,14 @@ describe("Updating records", () => {
 
   it("a model class can find a record with an Id and upate", done => {
     assertName(User.findByIdAndUpdate(joe._id, { name: "Alex" }), done);
+  });
+
+  it("a user can have their post count incremented by 1", () => {
+    User.update({ name: "Joe" }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: "Joe" }))
+      .then(user => {
+        assert(user.postCount === 1);
+        done();
+      });
   });
 });
