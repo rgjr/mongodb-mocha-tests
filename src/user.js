bfa6ff7278c2e22f035e45598fafa245ecc1da91
Raul Gutierrez <rdgutierrez@gmail.com>
2017-09-24 12:59:16 -0700

    added updating and virtual type tests

--- src/user.js
+++ src/user.js
@@ -11,8 +11,13 @@ const UserSchema = new Schema({
     },
     required: [true, "Name is required."]
   },
-  postCount: Number,
-  posts: [PostSchema]
+  posts: [PostSchema],
+  likes: Number
+});
+
+// Define a virtual field
+UserSchema.virtual("postCount").get(function() {
+  return this.posts.length;
 });
 
 const User = mongoose.model("user", UserSchema);
--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe("Updating records", () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: "Joe", postCount: 0 });
+    joe = new User({ name: "Joe", likes: 0 });
     joe.save().then(() => done());
   });
 
@@ -38,11 +38,13 @@ describe("Updating records", () => {
     assertName(User.findByIdAndUpdate(joe._id, { name: "Alex" }), done);
   });
 
-  it("a user can have their post count incremented by 1", () => {
-    User.update({ name: "Joe" }, { $inc: { postCount: 1 } })
-      .then(() => User.findOne({ name: "Joe" }))
+  it("a user can have their likes count incremented by 1", () => {
+    User.update({ name: "Joe" }, { $inc: { likes: 1 } })
+      .then(() => {
+        return User.findOne({ name: "Joe" });
+      })
       .then(user => {
-        assert(user.postCount === 1);
+        assert(user.likes === 1);
         done();
       });
   });
